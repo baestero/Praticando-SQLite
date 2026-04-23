43dbd1a4535f6ac9aa4d9851d097f1d0473e50fc
baestero <leonardobaestero@gmail.com>
2026-04-23 15:54:17 -0300

    fix: BETWEEN E IN

--- 016 - BETWEEN E IN/BETWEEN_E_IN.sql
+++ 016 - BETWEEN E IN/BETWEEN_E_IN.sql
@@ -8,3 +8,7 @@ SELECT * FROM produtos WHERE criado NOT BETWEEN  '2049-06' AND '2049-08';
 SELECT * FROM produtos WHERE categoria IN ('notebook', 'hardware');
 
 SELECT * FROM produtos WHERE categoria NOT IN ('notebook', 'hardware');
+
+SELECT * FROM produtos WHERE nome LIKE '%notebook%' OR categoria LIKE '%hardware%';
+
+SELECT * FROM produtos WHERE categoria = 'notebook' OR categoria = 'hardware'
